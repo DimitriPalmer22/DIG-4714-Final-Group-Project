949755f9987d280f8f1b3bdf7bfcbd5f44bbaa04
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2024-02-25 17:05:45 -0500

    Created Character & CharacterClass files

---
+++ .idea/.idea.DIG 4714 Final Group Project/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+K
+Assets/Scripts/Character.cs,2\a\2a92ad6d369f2e3472819be3728a7760ea5dcdb8
+P
+ Assets/Scripts/CharacterClass.cs,b\c\bc330be9cd5c2b958b91f3ae3beafba636599039
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    #region Fields
+
+    // Enum for character class
+    [SerializeField] private CharacterClass characterClass;
+    
+    #endregion
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
